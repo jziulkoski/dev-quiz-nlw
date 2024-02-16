1a39389c29aefa071dd547a76629541ed0a83e33
Juliana Ziulkoski <155642184+jziulkoski@users.noreply.github.com>
2024-02-16 16:49:19 -0300

    test naruto

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
 const perguntas = [
     {
-        pergunta: "O que significa a sigla HTML em JavaScript?",
+        pergunta: "Quem é o protagonista da série 'Naruto'?",
         respostas: [
-            "Hypertext Markup Language",
-            "High-Level Text Manipulation Language",
-            "Hyper Transfer Markup Language",
-        ],
-        correta: 0
-    },
-    {
-        pergunta: "Qual palavra-chave é usada para declarar uma variável em JavaScript?",
-        respostas: [
-            "variable",
-            "var",
-            "declare",
+            "Sasuke Uchiha",
+            "Naruto Uzumaki",
+            "Kakashi Hatake",
         ],
         correta: 1
     },
     {
-        pergunta: "Qual é a função do método 'addEventListener' em JavaScript?",
+        pergunta: "Qual é o nome do sensei de Naruto na equipe 7?",
         respostas: [
-            "Adicionar elementos ao documento",
-            "Registrar um ouvinte de eventos em um elemento",
-            "Remover um evento do documento",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Como você acessa o valor de um elemento em um array em JavaScript?",
-        respostas: [
-            "array.valor",
-            "array[valor]",
-            "array.value",
-        ],
-        correta: 1
-    },
-    {
-        pergunta: "Qual é a diferença entre '==' e '===' em JavaScript?",
-        respostas: [
-            "Nenhuma diferença",
-            "Comparação de valor apenas",
-            "Comparação de valor e tipo de dado",
+            "Jiraiya",
+            "Orochimaru",
+            "Kakashi Hatake",
         ],
         correta: 2
     },
     {
-        pergunta: "O que é uma função de callback em JavaScript?",
+        pergunta: "Qual é o nome da vila onde Naruto nasceu?",
         respostas: [
-            "Uma função que chama outra função",
-            "Uma função passada como argumento para outra função",
-            "Uma função que retorna um valor",
+            "Konohagakure",
+            "Sunagakure",
+            "Kirigakure",
         ],
-        correta: 1
+        correta: 0
     },
     {
-        pergunta: "O que é o conceito de 'hoisting' em JavaScript?",
+        pergunta: "Quem é considerado o rival de Naruto desde o início da série?",
         respostas: [
-            "Elevar um elemento na página",
-            "Mover uma função para o topo do escopo antes da execução",
-            "Adicionar um elemento ao final de um array",
+            "Sasuke Uchiha",
+            "Gaara",
+            "Neji Hyuga",
         ],
-        correta: 1
+        correta: 0
     },
     {
-        pergunta: "Qual método é usado para converter uma string em um número em JavaScript?",
+        pergunta: "Qual é o nome da raposa de nove caudas que está selada dentro de Naruto?",
         respostas: [
-            "parseInteger()",
-            "toNumber()",
-            "parseInt()",
+            "Shukaku",
+            "Matatabi",
+            "Kurama",
         ],
         correta: 2
     },
     {
-        pergunta: "Como você escreve um comentário de linha única em JavaScript?",
+        pergunta: "Qual é a técnica de assinatura de Naruto?",
         respostas: [
-            "// Este é um comentário",
-            "/* Este é um comentário */",
-            "' Este é um comentário",
+            "Chidori",
+            "Rasengan",
+            "Sharingan",
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Quem é o pai de Naruto?",
+        respostas: [
+            "Minato Namikaze",
+            "Kakashi Hatake",
+            "Hashirama Senju",
         ],
         correta: 0
     },
     {
-        pergunta: "O que é o operador ternário em JavaScript?",
+        pergunta: "Qual é a especialidade de Sasuke Uchiha?",
         respostas: [
-            "Um operador que trabalha com três operandos",
-            "Um operador utilizado para concatenar strings",
-            "Um operador de comparação de três valores",
+            "Taijutsu",
+            "Ninjutsu",
+            "Genjutsu",
         ],
-        correta: 0
+        correta: 1
+    },
+    {
+        pergunta: "Quem é o líder da Akatsuki?",
+        respostas: [
+            "Deidara",
+            "Pain",
+            "Itachi Uchiha",
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual é a ambição de Itachi Uchiha?",
+        respostas: [
+            "Se tornar Hokage",
+            "Proteger sua vila",
+            "Alcançar a paz através do genocídio",
+        ],
+        correta: 2
     },
 ];
 
